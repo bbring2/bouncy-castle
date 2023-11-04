3d53810b299ec3fd6d64ebab8d567b2f2a09ba7e
bbring <jennachoi27@gmail.com>
2023-11-04 21:44:20 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/bouncycastle/BouncyCastleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bouncycastle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BouncyCastleApplication
+
+fun main(args: Array<String>) {
+    runApplication<BouncyCastleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bouncycastle/BouncyCastleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bouncycastle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BouncyCastleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
